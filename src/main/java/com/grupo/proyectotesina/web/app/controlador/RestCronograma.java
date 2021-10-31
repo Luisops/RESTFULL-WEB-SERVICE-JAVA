f04ab537c479494e886196432d5a003aa9acac85
Luisops <luisjeremias33.3@gmail.com>
2021-10-30 22:15:47 -0500

    version 2

--- src/main/java/com/grupo/proyectotesina/web/app/controlador/RestCronograma.java
+++ src/main/java/com/grupo/proyectotesina/web/app/controlador/RestCronograma.java
@@ -1,5 +1,6 @@
 package com.grupo.proyectotesina.web.app.controlador;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.grupo.proyectotesina.web.app.modelo.Cronograma;
 import com.grupo.proyectotesina.web.app.service.FirebaseService;
 
@@ -25,26 +27,28 @@ public class RestCronograma {
 
 	 @Autowired
 	 FirebaseService firebaseServices;
-	
-	
-	 @GetMapping("/getCronogramaDetail") public Cronograma  getCronograma(@RequestHeader() 
-	 String Id_Cronograma) throws InterruptedException, ExecutionException{ return 
-	 firebaseServices.getCronogramaDeatils(Id_Cronograma);}
 	 
-	
-	
-	 @PostMapping("/postCronograma") public String postCronograma(@RequestBody
-	 Cronograma crono) throws InterruptedException, ExecutionException{ return
-	 firebaseServices.saveCronogramaDetails(crono); }
+	 @GetMapping("/getCronogramaDetail") 
+	 public Cronograma getCronograma(@RequestHeader() String Id_Cronograma) throws InterruptedException, ExecutionException{ 
+		 return firebaseServices.getCronogramaDeatils(Id_Cronograma);}
 	 
-	@PutMapping("/updateCronograma")
-	public String putCronograma(@RequestBody Cronograma cronograma)throws InterruptedException, ExecutionException {
-		return firebaseServices.updateUserDetails(cronograma);
-	}
+	 @GetMapping("/getListaDetalle") 
+	 public List<Cronograma> getAllDocument()throws InterruptedException, ExecutionException{	 
+		return firebaseServices.getAllDocuments(); 
+	 }
 
-	@DeleteMapping("/deleteCronograma")
-	public String deleteCronograma(@RequestHeader String Id_Cronograma) throws InterruptedException, ExecutionException{
+	 @PostMapping("/postCronograma") 
+	 public String postCronograma(@RequestBody Cronograma crono) throws InterruptedException, ExecutionException{ 
+		 return firebaseServices.saveCronogramaDetails(crono); }
+	 
+	 @PutMapping("/updateCronograma")
+	 public String putCronograma(@RequestBody Cronograma cronograma)throws InterruptedException, ExecutionException {
+		return firebaseServices.updateUserDetails(cronograma);
+	 }
+
+	 @DeleteMapping("/deleteCronograma")
+	 public String deleteCronograma(@RequestHeader String Id_Cronograma) throws InterruptedException, ExecutionException{
 		return firebaseServices.deletedCronogramaDetails(Id_Cronograma);
-	}
+	 }
 	
 }
--- src/main/java/com/grupo/proyectotesina/web/app/service/FirebaseService.java
+++ src/main/java/com/grupo/proyectotesina/web/app/service/FirebaseService.java
@@ -8,9 +8,14 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import com.grupo.proyectotesina.web.app.modelo.Cronograma;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /********SERVICIO****************************************************************************
  * @author Luis Muñoz Guerra 																*
@@ -63,6 +68,9 @@ public class FirebaseService {
 		}
 	}
 	
+	
+	
+	
 	/**
 	 * MODIFICA EL CRONOGRAMA Y LO ACTUALIZA A FIREBASE
 	 * @param cronog
@@ -76,6 +84,26 @@ public class FirebaseService {
 		ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection("cronograma").document(cronog.getId_Cronograma()).set(cronog);
 		return collectionApiFuture.get().getUpdateTime().toString();
 		
+	}
+	/**
+	 * OBTETIENE VARIOS DE DOCUMENTOS DE UNA COLECCIÓN
+	 * @return
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
+	public List<Cronograma> getAllDocuments() throws InterruptedException, ExecutionException{
+		//asynchronously retrieve all documents
+		Firestore dbFirestore = FirestoreClient.getFirestore();
+		ApiFuture<QuerySnapshot> collectionApiFuture = dbFirestore.collection("cronograma").get();
+		// future.get() blocks on response
+		List<Cronograma> cronList = new ArrayList<Cronograma>();
+		List<QueryDocumentSnapshot> documents = collectionApiFuture.get().getDocuments(); 
+		for (QueryDocumentSnapshot document : documents) {
+			Cronograma crono = document.toObject(Cronograma.class);
+			cronList.add(crono);
+			}					
+			return cronList;
+			
 	}
 	
 	public String deletedCronogramaDetails(String id_crono) throws InterruptedException, ExecutionException
